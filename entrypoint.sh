1d763ddb67c1a701711036fb2760d585aafa0d12
Thomas Hackel <thomas.hackel@solenix.ch>
2020-06-19 16:29:48 +0200

    Looping without direct set FAKETIME

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 echo -e "\nThe following FAKETIME* variables are set:"
 export|grep FAKE
 
+echo -e "\nRunning test without FAKETIME set here (current value: ${FAKETIME})"
+/bin/bash -c 'while [ $SECONDS -lt 5 ]; do date; sleep 1; done'
+
 echo -e "\nRunning test without FAKETIME_SHARED unset"
 FAKETIME="@2000-01-01 11:12:13" /bin/bash -c 'while [ $SECONDS -lt 5 ]; do date; sleep 1; done'
 
